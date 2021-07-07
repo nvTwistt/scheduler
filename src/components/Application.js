fc223a5b0d9f1b8503387a6cb885c63a21cd34c6
nv_twistt <mtamkee@icloud.com>
2021-07-06 20:44:03 -0600

    added a mini database to contain appointments, updated the html to display all the appointments, need to change the way the string is passed in with use state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,15 +1,114 @@
-import React from "react";
-
+import React , {useState} from "react";
+import DayList from "components/DayList";
 import "components/Application.scss";
-
+import Appointment from "components/Appointment";
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Matt",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Duncan",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Jose",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+];
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+const getAppointments = function() {
+  return appointments.map(i => {
+    return (
+      <Appointment
+      key={i.id}
+      interview={i.interview}
+      id={i.id}
+      time={i.time}
+      />
+    )
+  })
+}
+//const [day, setDay] = useState("Monday");
 export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+        <DayList days={days} day={"Monday"} setDay={() => "Monday"}/>
+
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {getAppointments()}
+        <Appointment key="last" id="last" time="5pm" />
       </section>
     </main>
   );
