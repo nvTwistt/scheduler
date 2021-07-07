688a0ba3279565278df337d65807d8db7fe74241
nv_twistt <mtamkee@icloud.com>
2021-07-06 20:50:05 -0600

    Added the day list item which shows the day and how many spots there are, added the styling for the file as well. Also implmeneted the unit tests

---
+++ src/components/DayListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+export default function DayListItem(props) {
+  const itemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  return (
+    <div className={itemClass} onClick={props.setDay}>
+      <h2><strong>{props.name}</strong></h2>
+      <p>{props.spots === 0 ? "no" : props.spots} spot{props.spots !== 1 && "s"}{" "}
+      remaining
+      </p>
+    </div>
+    // <li onClick={() => props.setDay(props.name)}>
+    //   <h2 className={itemClass}>{props.name}</h2>
+    //   <h3 className={itemClass}>{props.spots}</h3>
+    // </li>
+  );
+}
