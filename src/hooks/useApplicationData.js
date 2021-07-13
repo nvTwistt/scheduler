075326ed7e184c07023cba6b60f3ef0ab1e51f91
nv_twistt <mtamkee@icloud.com>
2021-07-13 13:03:52 -0600

    changed the book interview function to be a promise to help return the url, before it did not work on my last function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -54,11 +54,13 @@ export default function useApplicationData(props) {
 
   useEffect(() => {
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {  
+      
       //setState((prev) => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+      
       setState(({
         type: REGISTRATION,
         days: all[0].data,
@@ -67,22 +69,31 @@ export default function useApplicationData(props) {
       }))
     })
   }, []);
-    function bookInterview(id, interview) {
-      const appointment = {
-        ...state.appointments[id],
-        interview: {...interview}
-      }
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      }
-      const requestUrl = `http://localhost:8001/api/appointments/`+id;
-      return (
-        axios.put(requestUrl, {interview})
-        .then(() => {
-          setState({type: INTERVIEW, id, interview});
-        })
-        );
+    const bookInterview = (id, interview) => {
+      return new Promise((resolve, reject) => {
+        if (!interview.interviewer || interview.student === "") {
+          return reject();
+        }
+        const appointment = {
+              ...state.appointments[id],
+              interview: {...interview}
+            }
+        const requestUrl = `http://localhost:8001/api/appointments/`+id;
+        return axios.put(
+          requestUrl, appointment
+          )
+          .then(response => {
+            dispatchEvent({
+              type: INTERVIEW,
+              id, 
+              interview
+            })
+            resolve(response)
+          })
+          .catch(err => {
+            reject(err);
+          })
+      })
     }
     function cancelInterview(id) {
       const appointment = {
