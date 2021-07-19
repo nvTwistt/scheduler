c48673e79f5c2ef18c267028f2c6aec3b531004c
nv_twistt <mtamkee@icloud.com>
2021-07-19 15:22:55 -0700

    afixed bug where on edit, the interviewers name did not highlight, forgot to change interviewer to value during stretch exercise

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -100,7 +100,7 @@ export default function Appointment(props) {
         mode === EDIT && (
           <Form
             name={props.interview.student}
-            interviewer={props.interview.interviewer.id}
+            value={props.interview.interviewer.id}
             interviewers={props.interviewers}
             onSave={save}
             onCancel={back}
